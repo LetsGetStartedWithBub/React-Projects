cce1bcbbe7680946bcc0c21746fee26b4fc6d180
LetsGetStartedWithBub <letsgetstartedwithbub@gmail.com>
2022-12-19 23:42:15 +0530

    [ADDED] : Styling in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import {React, useState, useEffect} from 'react'
-import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import {Container, Navbar, Nav} from 'react-bootstrap';
+import '../static/styles/Navbar.css';
+import home from '../static/images/home.png';
 
-export const NavBar = () => {
+function NavBar() {
   const[activeLink, setActiveLink] = useState('home');
   const[scrolled, setScrolled] = useState(false);
   useEffect(() =>{
@@ -25,26 +27,18 @@ export const NavBar = () => {
     <Navbar bg="light" expand="lg" className = {scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={"logo"} alt = "logo" />
+          <img src={home} alt = "logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-        <span className="navbar-toggle-icon"></span>
-        </Navbar.Toggle>
+        
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="#link" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#link" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Blogs</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Youtube</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Behance</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Linkedin</NavDropdown.Item>
-            </NavDropdown>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
+export default NavBar;
